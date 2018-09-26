84a68222f6debea91e3d307a51df14bd3626979e
James Gibson <james.gibson@inboxinsight.com>
2018-09-26 14:26:51 +0100

    Add project files.

---
+++ IncorrectSyntaxNearTheKeywordAS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IncorrectSyntaxNearTheKeywordAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
