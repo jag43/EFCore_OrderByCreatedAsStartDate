84547c5266118bbf35ffc51108ef782a0b8c0c22
James Gibson <james.gibson@inboxinsight.com>
2018-09-28 10:54:35 +0100

    Added some output to `Program.cs`

--- IncorrectSyntaxNearTheKeywordAS/Program.cs
+++ IncorrectSyntaxNearTheKeywordAS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using IncorrectSyntaxNearTheKeywordAS.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace IncorrectSyntaxNearTheKeywordAS
@@ -12,20 +13,41 @@ namespace IncorrectSyntaxNearTheKeywordAS
 
         static async Task Main(string[] args)
         {
+            Log("Creating context...");
             var context = CreateContext(connectionString);
-
+            Log("Context created");
+            Log();
+            Log("Starting EnsureDeleted call...");
             context.Database.EnsureDeleted();
+            Log("EnsureDeleted call done.");
+            Log();
+            Log("Starting EnsureCreated call...");
             context.Database.EnsureCreated();
+            Log("EnsureCreated call done.");
 
+            Log("Inserting test data...");
             await new TestDataInserter(context).InsertTestDataAsync();
+            Log("Test data inserted.");
+            Log();
 
             var queryHandler = new QueryHandler(context);
 
+            Log("Starting query1 OrderByCount");
             var result1 = await queryHandler.ExecuteQueryOrderByCountAsync();
-            Console.WriteLine($"result1: {result1.Count} records.");
+            Console.WriteLine($"Query1 done. result: {result1.Count} records.");
 
-            var result2 = await queryHandler.ExecuteQueryOrderByCreatedAsync();
-            Console.WriteLine($"result2: {result2.Count} records.");
+            Log("Starting query2 OrderByCreated");
+            try
+            {
+                var result2 = await queryHandler.ExecuteQueryOrderByCreatedAsync();
+                Console.WriteLine($"result2: {result2.Count} records.");
+            }
+            catch(SqlException ex)
+            {
+                Log($"Error: {ex.Message}");
+                Log(ex.StackTrace);
+                Log();
+            }
 
             Console.ReadLine();
         }
@@ -37,6 +59,14 @@ namespace IncorrectSyntaxNearTheKeywordAS
                        .UseSqlServer(connectionString);
 
             return new DataContext(optionsBuilder.Options);
+        }
+
+        private static void Log(string s = null)
+        {
+            if (s != null)
+                Console.WriteLine(s);
+            else
+                Console.WriteLine();
         }
     }
 }
